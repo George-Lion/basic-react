fa32f65bde1f5b0812979b51c17ecaff7f84fb79
Jorge Leon <jorgeleonpicture@gmail.com>
2022-06-02 16:44:25 +0200

    start project

---
+++ src/components/testimonials.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "../styles/testimonials.css";
+
+export const Testimonials = () => {
+  return (
+    <div className="testimonial-content">
+      <img
+        className="testimonial-image"
+        src={require("../image/person1.jpg")}
+        alt="Emma_picture"
+      />
+
+      <div className="content-text-testimonials">
+        <p className="testimonial-name">Emma Bostian from Dubai</p>
+        <p className="testimonial-position">Software engineer at spotify</p>
+        <p className="testimonial-text">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five.
+        </p>
+      </div>
+    </div>
+  );
+};
